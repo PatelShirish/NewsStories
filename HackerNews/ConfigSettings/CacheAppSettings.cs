b458b3b62ac868df45949466d449df7676abdac2
PatelShirish <153219011+PatelShirish@users.noreply.github.com>
2023-12-10 22:40:24 +0000

    App settings

--- HackerNews/ConfigSettings/CacheAppSettings.cs
+++ HackerNews/ConfigSettings/CacheAppSettings.cs
@@ -1,1 +1,8 @@
-
+﻿namespace HackerNews.ConfigSettings
+{
+    public class CacheAppSettings
+    {
+        public int SlidingExpiration { get; set; }
+        public int AbsoluteExpiration { get; set; }
+    }
+}
---
+++ HackerNews/ConfigSettings/StoryUrlsAppSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace HackerNews.ConfigSettings
+{
+    public class StoryUrlsAppSettings
+    {
+        public string BestStoriesUrl { get; set; }
+        public string StoryByIdUrl { get; set;}
+    }
+}
