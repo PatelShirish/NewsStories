64f294b8f51319da07a1cfb49834c1ea1517da77
PatelShirish <153219011+PatelShirish@users.noreply.github.com>
2023-12-10 22:41:53 +0000

    Added HackerController

--- HackerNews/Controllers/HackerController.cs
+++ HackerNews/Controllers/HackerController.cs
@@ -1,1 +1,32 @@
+using AutoMapper;
+using HackerNews.Models;
+using Microsoft.AspNetCore.Mvc;
+namespace HackerNews.Controllers
+{
+    [ApiController]
+    [Route("HackerNews")]
+    public class HackerController : ControllerBase
+    {
+        private readonly IMapper _mapper;
+        private readonly IHackerService _hackerService;
 
+        public HackerController(IHackerService hackerService, IMapper mapper)
+        {
+            _hackerService = hackerService;
+            _mapper = mapper;
+        }
+
+        /// <summary>
+        /// Gets Best n Hacker News stories 
+        /// </summary>
+        /// <param name="count">Number of best stories</param>
+        /// <returns>Best n stories</returns>
+        [HttpGet]
+        [Route("GetBestStories")]
+        public async Task<IEnumerable<StoryResponse>> GetBestStories(int count = 10)
+        {
+            IEnumerable<Story> data = await _hackerService.GetBestStoriesAsync(count);
+            return _mapper.Map<IEnumerable<StoryResponse>>(data);
+        }
+    }
+}
